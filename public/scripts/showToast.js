f74fbd610e707ccacf19385e086d47d147a7c672
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-03-06 16:48:12 +0800

    Update showToast.js

--- public/scripts/showToast.js
+++ public/scripts/showToast.js
@@ -16,7 +16,7 @@ function showToast(message, type, duration = 3000) {
 window.showToast = showToast;
 
 // Show toast after successful sign up
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = () => {
     const toastMessage = localStorage.getItem("toastMessage");
     const toastType = localStorage.getItem("toastType");
 
@@ -25,4 +25,4 @@ document.addEventListener("DOMContentLoaded", () => {
         localStorage.removeItem("toastMessage");
         localStorage.removeItem("toastType");
     }
-});
+};
