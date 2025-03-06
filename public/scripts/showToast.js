e27432b3ac3b1a1a0a5f94f1b7cf7b191f9212e8
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-03-06 14:20:27 +0800

    Update showToast.js

--- public/scripts/showToast.js
+++ public/scripts/showToast.js
@@ -1,12 +1,13 @@
-function showToast(message, duration = 3000) {
-    const toast = document.getElementById('toast');
-    const toastMessage = document.getElementById('toast-message');
+function showToast(message, type="success", duration=3000) {
+    const toast = document.getElementById("toast");
+    const toastMessage = document.getElementById("toast-message");
     
     toastMessage.textContent = message;
-    toast.classList.add('show');
+    toast.classList.remove("success", "error");
+    toast.classList.add("show");
     
     setTimeout(() => {
-      toast.classList.remove('show');
+      toast.classList.remove("show");
     }, duration);
 }
 
