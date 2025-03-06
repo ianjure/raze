97820c7663e8cd954dc2e61925ec6219612082c6
ianjure <ianjuremacalisang2@gmail.com>
2025-03-06 22:19:14 +0800

    fixed favicon bug

--- public/scripts/interface/updateFavicon.js
+++ public/scripts/interface/updateFavicon.js
@@ -1,6 +1,6 @@
 function updateFavicon() {
     const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const faviconHref = darkMode ? "../assets/raze-light-1.svg" : "../assets/raze-dark-1.svg"; // Switched if necessary
+    const faviconHref = darkMode ? "../assets/logo/raze-light-1.svg" : "../assets/logo/raze-dark-1.svg";
 
     // Remove existing favicon(s)
     document.querySelectorAll("link[rel='icon']").forEach(el => el.remove());
