ef066b83ea953a32c49e83ace55f9e8d0f6fade2
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-03-06 16:49:30 +0800

    Update adminDashboard.js

--- public/scripts/adminDashboard.js
+++ public/scripts/adminDashboard.js
@@ -1,5 +1,5 @@
 // To display user information as admin
-document.addEventListener("DOMContentLoaded", async () => {
+window.onload = () => {
 
     // Get the token, username, and role from local storage
     const token = localStorage.getItem("token");
