45fd08be1e342464e7210bff6e6c02bd3eb09503
ianjure <ianjuremacalisang2@gmail.com>
2025-03-07 09:29:08 +0800

    optimized static routes

--- src/routes/static.route.js
+++ src/routes/static.route.js
@@ -3,6 +3,17 @@ const path = require("path");
 
 const router = express.Router();
 
+// Admin static routes
+router.get("/admin", (req, res) => {
+    res.redirect("/admin/login");
+});
+router.get("/admin/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/admin/admin-login.html"));
+});
+router.get("/admin/:username", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/admin/admin-dashboard.html"));
+});
+
 // User static routes
 router.get("/", (req, res) => {
     res.redirect("/login");
@@ -14,20 +25,7 @@ router.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/user/user-login.html"));
 });
 router.get("/:username", (req, res) => {
-    const username = req.params.username;
     res.sendFile(path.join(__dirname, "../../public/user/user-dashboard.html"));
-});
-
-// Admin static routes
-router.get("/admin", (req, res) => {
-    res.redirect("/admin/login");
-});
-router.get("/admin/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/admin/admin-login.html"));
-});
-router.get("/admin/:username", (req, res) => {
-    const username = req.params.username;
-    res.sendFile(path.join(__dirname, "../../public/admin/admin-dashboard.html"));
 });
 
 module.exports = router;
